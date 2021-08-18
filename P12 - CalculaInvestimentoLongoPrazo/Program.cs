c44b262c8f71e338286b805f2fc883c14e2b4b50
lucas41 <32311187+lucas41@users.noreply.github.com>
2021-08-18 19:03:39 -0300

    Primeira parte completa

---
+++ P12 - CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12___CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executanto o projeto 12");
+
+            double valinvestido = 1000.00;
+            double fatorrendimento = 1.0036;
+
+            for (int contano = 1; contano <=5; contano++)
+            {
+                for (int contmes = 1; contmes <= 12; contmes++)
+                {
+                    valinvestido *= fatorrendimento;
+                }
+
+                fatorrendimento += 0.0010;
+            }
+         
+          Console.WriteLine("Ao termino do investimento você tera R$: " + valinvestido);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P13 - For encadeado/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13___For_encadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 13");
+
+            for (int contlinha = 0; contlinha < 10; contlinha++)
+            {
+                for (int contcoluna = 0; contcoluna < 10; contcoluna++)
+                {
+                    Console.Write("*");
+                    if (contcoluna >= contlinha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
