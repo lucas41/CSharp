95e00a6b77407d0adec18f839d2009ad4b82ef8a
lucas41 <32311187+lucas41@users.noreply.github.com>
2021-08-25 17:05:00 -0300

    poo 25-08

---
+++ POO/04 - ByteBank/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contabruno = new ContaCorrente();
+
+            contabruno.titular = "bruno";
+
+            Console.WriteLine(contabruno.saldo);
+            bool resultadoSaque = contabruno.Sacar(50);
+            if (resultadoSaque == true)
+            {
+                Console.WriteLine("Saque efetuado com sucesso");
+            } else
+            {
+                Console.WriteLine("Falha ao efetuar saque");
+            }
+
+            Console.WriteLine(resultadoSaque);
+            Console.WriteLine(contabruno.saldo);
+
+            contabruno.Depositar(500);
+            Console.WriteLine(contabruno.saldo);
+
+
+            ContaCorrente contagabriela = new ContaCorrente();
+
+            contagabriela.titular = "gabi";
+
+            Console.WriteLine("Saldo do Bruno: " + contabruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contagabriela.saldo);
+
+           bool resultado = contabruno.transferir(200, contagabriela);
+
+            if (resultado == true)
+            {
+                Console.WriteLine("transferencia efetuado com sucesso");
+
+            } else
+            {
+                Console.WriteLine("Falha na transferencia");
+            }
+
+            Console.WriteLine("Saldo do Bruno: " + contabruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contagabriela.saldo);
+            Console.ReadLine();
+        }
+    }
+}
