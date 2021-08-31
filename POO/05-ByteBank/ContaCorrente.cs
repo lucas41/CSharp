ffd167fa3049d70a0152a7eaabb41b180e93ec6b
lucas41 <32311187+lucas41@users.noreply.github.com>
2021-08-31 10:50:11 -0300

    atualizacao de hoje

---
+++ POO/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,48 @@
+﻿
+using _05_ByteBank;
+public class ContaCorrente
+{
+    public Cliente titular;
+    public Cliente cpf;
+    public int numeroAgencia;
+    public int numeroConta;
+    public double saldo = 100;
+
+
+
+    public bool Sacar(double valor)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        } else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool transferir(double valor,ContaCorrente contaDestino)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        } else
+        {
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
+
+
+
+
+
