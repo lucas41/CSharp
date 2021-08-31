4135496b5cc7e6f5fbfe84faa7017e8988fbab78
lucas41 <32311187+lucas41@users.noreply.github.com>
2021-08-31 14:32:57 -0300

    31-08 -2

--- POO/05-ByteBank/Program.cs
+++ POO/05-ByteBank/Program.cs
@@ -10,21 +10,26 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente gabriela = new Cliente();
+           // Cliente gabriela = new Cliente();
 
-            gabriela.nome = "Gabriela";
-            gabriela.profissao = "Dev c#";
-            gabriela.cpf = "456.543.243-20";
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Dev c#";
+            //gabriela.cpf = "456.543.243-20";
 
             ContaCorrente conta = new ContaCorrente();
 
-            conta.titular = gabriela;
+            //conta.titular = gabriela;
+            //conta.titular = new Cliente();
+            //conta.titular.cpf = "432.543.432 - 30";
+
+            // caso a referencia não aponte para lugar nenhum havera um erro de exceção
+
             conta.saldo = 500;
             conta.numeroAgencia = 563;
             conta.numeroAgencia = 234;
 
-            Console.WriteLine(gabriela.nome);
-            Console.WriteLine(conta.titular.nome);
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.cpf);
             Console.ReadLine();
         }
     }
